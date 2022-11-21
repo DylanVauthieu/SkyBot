815017fc794c8af8415e4875d15a21a4cf623981
Skylost <dylan.vauthieu@epitech.eu>
2022-11-21 05:12:56 +0100

    Add : Command Handler

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const discord = require("discord.js");
-const bot = new discord.Client({intents: 3276799});
+const intents = new discord.IntentsBitField(3276799);
+const bot = new discord.Client({intents});
+const loadCommands = require("./loaders/LoadCommands");
 const config = require("./config");
 
+bot.commands = new discord.Collection();
+
 bot.login(config.token);
+loadCommands(bot);
+
+bot.on("messageCreate", async message => {
+
+    if (message.content === "!ping") return bot.commands.get("ping").run(bot, message);
+});
 
 bot.on("ready", async () => {
 
     console.log(`${bot.user.tag} est en ligne !`);
-})
+});
