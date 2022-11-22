c2d4de9aed4ce9a41c3a007aae39e4caeaee4fe9
DylanVauthieu <dylan.vauthieu@epitech.eu>
2022-11-22 12:11:41 +0100

    Add : Slash command support

--- Events/ready.js
+++ Events/ready.js
@@ -1,6 +1,8 @@
 const discord = require("discord.js");
+const LoadSlashCommands = require("../loaders/LoadSlashCommands");
 
-module.exports = async (bot, message) => {
+module.exports = async bot => {
 
+    await LoadSlashCommands(bot);
     console.log(`${bot.user.tag} est en ligne !`);
 }
