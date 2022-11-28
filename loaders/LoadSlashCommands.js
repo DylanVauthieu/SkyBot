c8fddf74fbda61771d35125b175183d1f8f008a8
Skylost <dylan.vauthieu@epitech.eu>
2022-11-28 03:51:02 +0100

    Fix : LoadSlashCommand had some bugs -> fixed

--- loaders/LoadSlashCommands.js
+++ loaders/LoadSlashCommands.js
@@ -11,18 +11,17 @@ module.exports = async bot => {
         .setName(command.name)
         .setDescription(command.description)
         .setDMPermission(command.dm)
-        .setDefaultMemberPermissions(command.permission === 'None' ? null : command.permission);
+        .setDefaultMemberPermissions(command.permission === "none" ? null : command.permission);
 
-        if (command.options?.lenght >= 1) {
-            for (let i = 0; i < command.options.lenght; i++) {
-                slashcommand[`add${command.options[i].type.slice(0, 1).tolowerCase() + command.options[i].type.slice(1, command.options[i].type.lenght)}option`]
-                (option => options.setName(command.options[i].name).setDescription(command.options[i].description).setRequired(command.options[i].required));
+        if (command.options?.length >= 1) {
+            for(let i = 0; i < command.options.length; i++) {
+                slashcommand[`add${command.options[i].type.slice(0, 1).toUpperCase() + command.options[i].type.slice(1, command.options[i].type.length)}Option`](option => option.setName(command.options[i].name).setDescription(command.options[i].description).setRequired(command.options[i].required));
             }
         }
 
 
         await commands.push(slashcommand);
-    });
+    })
 
     const rest = new REST({version: "10"}).setToken(bot.token);
 
