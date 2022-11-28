f625b62f2975820525dc5c6f13e67ee0fffb5279
Skylost <dylan.vauthieu@epitech.eu>
2022-11-28 04:30:39 +0100

    Change : Code more beautiful

--- commands/ban.js
+++ commands/ban.js
@@ -22,24 +22,30 @@ module.exports = {
     async run(bot, message, args) {
         try {
             let user = await bot.users.fetch(args._hoistedOptions[0].value)
-            if (!user) return message.reply("No member to ban !")
-
             let member = message.guild.members.cache.get(user.id)
             let reason = args.getString("reason")
-            if (!reason) reason = "No reason given for the ban."
 
-            if ((await message.guild.fetchOwner()).id === user.id) return message.reply("You can't ban the Owner.")
-            if (message.user.id === user.id) return message.reply("Why you want to ban yourself ?")
-            if (member && !member.bannable) return message.reply("I can't ban this user.")
-            if (member && message.member.roles.highest.comparePositionTo(member.roles.highest) <= 0) return message.reply("You can't ban this user.")
-            if ((await message.guild.bans.fetch()).get(user.id)) return message.reply("This user are already ban.")
+            if (!user)
+                return message.reply("No member to ban !");
+            if (!reason)
+                reason = "No reason given for the ban.";
+            if ((await message.guild.fetchOwner()).id === user.id)
+                return message.reply("You can't ban the Owner.");
+            if (message.user.id === user.id)
+                return message.reply("Why you want to ban yourself ?");
+            if (member && !member.bannable)
+                return message.reply("I can't ban this user.");
+            if (member && message.member.roles.highest.comparePositionTo(member.roles.highest) <= 0)
+                return message.reply("You can't ban this user.");
+            if ((await message.guild.bans.fetch()).get(user.id))
+                return message.reply("This user are already ban.");
 
             try {await user.sent(`You have been ban from *${message.guild.name}* by *${message.user.tag}* for the reason : \`${reason}\``)} catch(err) {}
 
-            await message.reply(`${message.user} have been banned ${user.tag} for the reason : \`${reason}\``)
-            await message.guild.bans.create(user.id, {reason: reason})
+            await message.reply(`${message.user} have been banned ${user.tag} for the reason : \`${reason}\``);
+            await message.guild.bans.create(user.id, {reason: reason});
         } catch (err) {
-            return message.reply("No member to ban !")
+            return message.reply("No member to ban !");
         }
     }
 }
