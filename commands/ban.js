9c38d6dc32358845b6661c0a784a4312bdfdad1f
Skylost <dylan.vauthieu@epitech.eu>
2022-11-28 03:49:33 +0100

    Add : Ban and Kick

---
+++ commands/ban.js
@@ -0,0 +1,45 @@
+const discord = require("discord.js");
+
+module.exports = {
+    name: "ban",
+    description: "Ban a member",
+    permission: discord.PermissionFlagsBits.BanMembers,
+    dm: false,
+    options: [
+        {
+            type: "user",
+            name: "member",
+            description: "The member to ban",
+            required: true
+        }, {
+            type: "string",
+            name: "reason",
+            description: "The reason for ban this member",
+            required: false
+        }
+    ],
+
+    async run(bot, message, args) {
+        try {
+            let user = await bot.users.fetch(args._hoistedOptions[0].value)
+            if (!user) return message.reply("No member to ban !")
+
+            let member = message.guild.members.cache.get(user.id)
+            let reason = args.getString("reason")
+            if (!reason) reason = "No reason given for the ban."
+
+            if ((await message.guild.fetchOwner()).id === user.id) return message.reply("You can't ban the Owner.")
+            if (message.user.id === user.id) return message.reply("Why you want to ban yourself ?")
+            if (member && !member.bannable) return message.reply("I can't ban this user.")
+            if (member && message.member.roles.highest.comparePositionTo(member.roles.highest) <= 0) return message.reply("You can't ban this user.")
+            if ((await message.guild.bans.fetch()).get(user.id)) return message.reply("This user are already ban.")
+
+            try {await user.sent(`You have been ban from *${message.guild.name}* by *${message.user.tag}* for the reason : \`${reason}\``)} catch(err) {}
+
+            await message.reply(`${message.user} have been banned ${user.tag} for the reason : \`${reason}\``)
+            await message.guild.bans.create(user.id, {reason: reason})
+        } catch (err) {
+            return message.reply("No member to ban !")
+        }
+    }
+}
---
+++ commands/kick.js
@@ -0,0 +1,40 @@
+const discord = require("discord.js");
+
+module.exports = {
+    name: "kick",
+    description: "Kick a member",
+    permission: discord.PermissionFlagsBits.KickMembers,
+    dm: false,
+    options: [
+        {
+            type: "user",
+            name: "member",
+            description: "The member to kick",
+            required: true
+        }, {
+            type: "string",
+            name: "reason",
+            description: "The reason for kick this member",
+            required: false
+        }
+    ],
+
+    async run(bot, message, args) {
+        let user = args.getUser("member")
+        let member = message.guild.members.cache.get(user.id)
+        let reason = args.getString("reason")
+
+        if (!user) return message.reply("No member to kick !")
+        if (!member) return message.reply("No member to kick.")
+        if (!reason) reason = "No reason given for the kick."
+        if ((await message.guild.fetchOwner()).id === user.id) return message.reply("You can't kick the Owner.")
+        if (message.user.id === user.id) return message.reply("Why you want to kick yourself ?")
+        if (member && !member.kickable) return message.reply("I can't kick this user.")
+        if (member && message.member.roles.highest.comparePositionTo(member.roles.highest) <= 0) return message.reply("You can't kick this user.")
+
+        try {await user.sent(`You have been kick from *${message.guild.name}* by *${message.user.tag}* for the reason : \`${reason}\``)} catch(err) {}
+
+        await message.reply(`${message.user} have been kicked ${user.tag} for the reason : \`${reason}\``)
+        await member.kick(reason)
+    }
+}
