1c017791fa805337806f65ed4f412d0ca93ac798
DylanVauthieu <dylan.vauthieu@epitech.eu>
2022-12-01 01:41:00 +0100

    Change : Autocomplete added

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -2,6 +2,13 @@ const discord = require("discord.js");
 
 module.exports = async (bot, interaction) => {
 
+    if (interaction.type === discord.InteractionType.ApplicationCommandAutocomplete) {
+
+        let entry = interaction.options.getFocused();
+        let choices = bot.commands.filter(cmd => cmd.name.includes(entry))
+
+        await interaction.respond(entry === "" ? bot.commands.map(cmd => ({name: cmd.name, value: cmd.name})) : choices.map(choice => ({name: choice.name, value: choice.name})))
+    }
     if (interaction.type === discord.InteractionType.ApplicationCommand) {
 
         let command = require(`../commands/${interaction.commandName}`)
